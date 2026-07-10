34bc73cf29b5a32c120f03e128ce60096c6a8057
RICKY-gmdev <rickymessi980@gmail.com>
2026-07-10 10:47:33 +0530

    requirements.txt fix

--- App/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/App.AssemblyInfo.cs
+++ App/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/App.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b085ab254af7432e456d3c99e6fa46874f0002df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+033410fd4c280beb569695c1ab77013321674016")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
